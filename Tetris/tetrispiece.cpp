0dc1dceffb3c58572487e2133e73f76dbe743a91
Oleg <sed-800@yandex.ru>
2017-12-22 18:00:46 +0300

    Наполнение кодом

--- Tetris/tetrispiece.cpp
+++ Tetris/tetrispiece.cpp
@@ -1,6 +1,29 @@
 #include "tetrispiece.h"
 
-TetrisPiece::TetrisPiece()
-{
+#include <stdlib.h>
+#include <QtCore>
 
+void TetrisPiece::setRandomShape()
+{
+    setShape(TetrixShape(qrand()%7 + 1));
+}
+
+void TetrisPiece::setShape(TetrixShape shape)
+{
+    static const int coordsTable[8][4][2] = {
+        { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } },
+        { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1 } },
+        { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } },
+        { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } },
+        { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } },
+        { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } },
+        { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } },
+        { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } }
+    };
+
+    for (int i = 0; i < 4 ; i++) {
+        for (int j = 0; j < 2; ++j)
+            coords[i][j] = coordsTable[shape][i][j];
+    }
+    pieceShape = shape;
 }
--- Tetris/tetrispiece.h
+++ Tetris/tetrispiece.h
@@ -1,20 +1,30 @@
 #ifndef TETRISPIECE_H
 #define TETRISPIECE_H
 
-enum TetrixShape { NoShape, ZShape, SShape, LineShape, TShape, SquareShape,
+enum class TetrixShape { NoShape, ZShape, SShape, LineShape, TShape, SquareShape,
                    LShape, MirroredLShape };
 
 class TetrisPiece
 {
 public:
-    TetrisPiece();
+    TetrisPiece(){ setShape(TetrixShape::NoShape); }
 
     void setRandomShape();
     void setShape(TetrixShape shape);
 
-    TetrixShape shape() const;
+    TetrixShape shape() const { return pieceShape; }
+    int x(int index) const { return coord[index][0]; }
+    int y(int index) const { return coord[index][1]; }
+    int minX() const;
+    int maxX() const;
+    int minY() const;
+    int maxY() const;
+    TetrisPiece rotatedLeft() const;
+    TetrisPiece rotatedRight() const;
 
 private:
+    void setX(int index , int x) { coord[index][0] = x; }
+    void setY(int index , int y) { coord[index][1] = y; }
 
     TetrixShape pieceShape;
     int coord[4][2];
