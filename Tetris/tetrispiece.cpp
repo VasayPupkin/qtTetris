1512b4716ae6327b27cac0f529fd531ae529db39
Oleg <sed-800@yandex.ru>
2017-12-26 08:50:16 +0300

    Добавлена реализация методов в tetrispiecce.

--- Tetris/tetrispiece.cpp
+++ Tetris/tetrispiece.cpp
@@ -27,3 +27,61 @@ void TetrisPiece::setShape(TetrixShape shape)
     }
     pieceShape = shape;
 }
+
+int TetrisPiece::minX() const
+{
+    int min = coord[0][0];
+    for(int i = 1; i < 4 ; ++i)
+        min = qMin(min , coord[i][0]);
+    return min;
+}
+
+int TetrisPiece::maxX() const
+{
+    int max = coord[0][0];
+    for(int i = 1; i < 4 ; ++i)
+        max = qMax(min , coord[i][0]);
+    return max;
+}
+
+int TetrisPiece::minY() const
+{
+    int min = coord[0][1];
+    for(int i = 1; i < 4 ; ++i)
+        min = qMin(min , coord[i][1]);
+    return min;
+}
+
+int TetrisPiece::maxY() const
+{
+    int max = coord[0][1];
+    for(int i = 1; i < 4 ; ++i)
+        max = qMax(min , coord[i][1]);
+    return max;
+}
+
+TetrisPiece TetrisPiece::rotatedLeft() const
+{
+    if(pieceShape == TetrixShape::SquareShape)
+        return *this;
+    TetrisPiece result;
+    result.pieceShape = pieceShape;
+    for(int i = 0; i < 4 ; ++i){
+        result.setX(i , y(i));
+        result.setY(i , -x(i));
+    }
+    return result;
+}
+
+TetrisPiece TetrisPiece::rotatedRight() const
+{
+    if(pieceShape == TetrixShape::SquareShape)
+        return *this;
+    TetrisPiece result;
+    result.pieceShape = pieceShape;
+    for(int i = 0; i < 4 ; ++i){
+        result.setX(i , -y(i));
+        result.setY(i , x(i));
+    }
+    return result;
+}
